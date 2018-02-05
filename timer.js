4d81bb4e59c7dc272984bae172b4263128c249f3
YasinChan <yasinchan2016@gmail.com>
2018-02-06 00:33:52 +0800

    project init

---
+++ timer.js
@@ -0,0 +1,24 @@
+/*
+* 计时倒计时插件
+*
+* */
+(function () {
+
+    function Timer(el, opts) {
+        this.el = el;
+        this.opts = opts;
+        this.init();
+    }
+    Timer.prototype = {
+        init: function () {
+            this.reqAniFra = null;
+
+        },
+        start: function () {
+
+        }
+    }
+
+
+
+})()
